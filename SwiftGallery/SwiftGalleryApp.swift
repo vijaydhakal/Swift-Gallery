c8da3ee533e01a8e0dad5630fed5517a796461a2
Vijay Dhakal <vijaydhakal48@gmail.com>
2025-11-02 09:01:12 +0100

    Initial Commit

---
+++ SwiftGallery/SwiftGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGalleryApp.swift
+//  SwiftGallery
+//
+//  Created by Vijay Dhakal on 02/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
