06a92ed0fd841dbefe169304d668799dbf724b7d
Vijay Dhakal <vijaydhakal48@gmail.com>
2025-11-02 09:51:49 +0100

    splash screen of hta app

---
+++ SwiftGallery/habit_tracker_app/SplashScreen.swift
@@ -0,0 +1,36 @@
+//
+//  SplashScreen.swift
+//  SwiftGallery
+//
+//  Created by Vijay Dhakal on 02/11/25.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    
+   var body: some View {
+        GeometryReader{ geo in
+            ZStack {
+                Image("HTA_Splash_Screen")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("Welcome to celestial Habit")
+                        .font(.system(size: 34, weight: .bold))
+                        .foregroundColor(.black.opacity(0.8))
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 40)
+                        .textCase(.uppercase)
+                    Spacer()
+                }
+                .padding(.top, geo.size.height * 0.08)
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
